c0f16d20a6112b607676da67edeb706f56e2c5ae
nemo <nemosource@gmail.com>
2018-04-13 19:10:51 +0900

    일반 자바 프로젝트로 변경

---
+++ gson/src/main/java/nemo/google/gson/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link nemo.google.gson.Gson}.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package nemo.google.gson.annotations;
---
+++ gson/src/main/java/nemo/google/gson/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package provides the {@link nemo.google.gson.Gson} class to convert Json to Java and
+ * vice-versa.
+ *
+ * <p>The primary class to use is {@link nemo.google.gson.Gson} which can be constructed with
+ * {@code new Gson()} (using default settings) or by using {@link nemo.google.gson.GsonBuilder}
+ * (to configure various options such as using versioning and so on).</p>
+ *
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package nemo.google.gson;
